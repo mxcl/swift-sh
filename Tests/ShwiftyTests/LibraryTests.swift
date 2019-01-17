7e9b37d7a607f9a3ac4f1d11759560a5c4bba54c
Max Howell <mxcl@me.com>
2019-01-17 12:55:43 -0500

    Fix Linux test

--- Tests/ShwiftyTests/LibraryTests.swift
+++ Tests/ShwiftyTests/LibraryTests.swift
@@ -3,7 +3,11 @@ import XCTest
 
 class LibraryTests: XCTestCase {
     func testStrerror() {
-        //NOTE does this get localized? If fails for you please open a ticket! https://github.com/mxcl/swift-sh/issues
+    #if os(macOS)
+        // If fails for you please open a ticket! https://github.com/mxcl/swift-sh/issues/new
         XCTAssertEqual(strerror(ERANGE), "Result too large (34)")
+    #else
+        XCTAssert(strerror(ERANGE).hasSuffix("(34)"))
+    #endif
     }
 }
