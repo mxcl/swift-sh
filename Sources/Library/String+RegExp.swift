4328455c09dcd253c7583b392ce057e33f7b5b40
Max Howell <mxcl@me.com>
2019-01-30 12:07:06 -0500

    Fixes for our NSRange conversion functions

--- Sources/Library/String+RegExp.swift
+++ Sources/Library/String+RegExp.swift
@@ -1,41 +1,14 @@
 import Foundation
 
+public extension NSRegularExpression {
+    func firstMatch(in str: String) -> NSTextCheckingResult? {
+        let range = NSRange(location: 0, length: str.utf16.count)
+        return firstMatch(in: str, range: range)
+    }
+}
+
 public extension String {
-    /// An `NSRange` that represents the full range of the string.
-    var nsRange: NSRange {
-        return NSRange(location: 0, length: utf16.count)
-    }
-
-    /// Returns a substring with the given `NSRange`,
-    /// or `nil` if the range can't be converted.
-    func substring(with nsrange: NSRange) -> String? {
-        guard let range = Range(nsrange) else { return nil }
-    #if swift(>=5)
-        let start = UTF16View.Index(encodedOffset: range.lowerBound)
-        let end = UTF16View.Index(encodedOffset: range.upperBound)
-    #else
-        let start = UTF16Index(encodedOffset: range.lowerBound)
-        let end = UTF16Index(encodedOffset: range.upperBound)
-    #endif
-        return String(utf16[start..<end])
-    }
-
-    /// Returns a range equivalent to the given `NSRange`,
-    /// or `nil` if the range can't be converted.
-    func range(from nsrange: NSRange) -> Range<Index>? {
-        guard let range = Range(nsrange) else { return nil }
-    #if swift(>=5)
-        let utf16Start = UTF16View.Index(encodedOffset: range.lowerBound)
-        let utf16End = UTF16View.Index(encodedOffset: range.upperBound)
-    #else
-        let utf16Start = UTF16Index(encodedOffset: range.lowerBound)
-        let utf16End = UTF16Index(encodedOffset: range.upperBound)
-    #endif
-
-        guard let start = Index(utf16Start, within: self),
-            let end = Index(utf16End, within: self)
-            else { return nil }
-
-        return start..<end
+    subscript(range: NSRange) -> Substring {
+        return self[Range(range, in: self)!]
     }
 }
