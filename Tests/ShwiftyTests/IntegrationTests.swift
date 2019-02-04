5f1fd961d94e5d3d99f970d913f352fa2e962e05
Max Howell <mxcl@me.com>
2019-02-04 03:10:13 +0000

    Add Examples/

--- Tests/ShwiftyTests/IntegrationTests.swift
+++ Tests/ShwiftyTests/IntegrationTests.swift
@@ -112,57 +112,10 @@ class RunIntegrationTests: XCTestCase {
             """, arg: "3")
     }
 
-    func testNSHipsterExample() {
-        XCTAssertRuns(exec: """
-            import DeckOfPlayingCards  // @NSHipster ~> 4.0.0
-            import PlayingCard
-            import Cycle  // @NSHipster == bb11e28
-
-            class Player {
-                var name: String
-                var hand: [PlayingCard] = []
-
-                init(name: String) {
-                    self.name = name
-                }
-            }
-
-            extension Player: CustomStringConvertible {
-                var description: String {
-                    var description = "\\(name):"
-
-                    let cardsBySuit = Dictionary(grouping: hand) { $0.suit }
-                    for (suit, cards) in cardsBySuit.sorted(by: { $0.0 > $1.0 }) {
-                        description += "\\t\\(suit) "
-                        description += cards.sorted(by: >)
-                                            .map{ "\\($0.rank)" }
-                                            .joined(separator: " ")
-                        description += "\\n"
-                    }
-
-                    return description
-                }
-            }
-
-            var deck = Deck.standard52CardDeck()
-            deck.shuffle()
-
-            var north = Player(name: "North")
-            var west = Player(name: "West")
-            var east = Player(name: "East")
-            var south = Player(name: "South")
-
-            let players = [north, east, west, south]
-            var round = players.cycled()
-
-            while let card = deck.deal(), let player = round.next() {
-                player.hand.append(card)
-            }
-
-            for player in players {
-                print(player)
-            }
-            """)
+    func testNSHipsterExample() throws {
+        let path = Path(#file)!.parent.parent.parent.Examples.cards
+        let code = try StreamReader(path: path).dropFirst().joined(separator: "\n")
+        XCTAssertRuns(exec: code)
     }
 
     func testRelativePath() throws {
