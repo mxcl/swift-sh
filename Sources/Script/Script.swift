f9055697f6ac691ab52b70a8b625cde607ff55ec
wti <wti@users.noreply.github.com>
2022-10-31 12:07:13 -0700

    Extend script version #if fallback to 5.8, 5.9, and 6.0

--- Sources/Script/Script.swift
+++ Sources/Script/Script.swift
@@ -259,8 +259,12 @@ let swiftVersion: String = {
     } catch {
         assert(false)  // shouldn't happen during testing so let’s catch it
     }
-#if swift(>=5.8)
-    return "5.8+"
+#if swift(>=6.0)
+    return "6.0"
+#elseif swift(>=5.9)
+    return "5.9"
+#elseif swift(>=5.8)
+    return "5.8"
 #elseif swift(>=5.7)
     return "5.7"
 #elseif swift(>=5.6)
