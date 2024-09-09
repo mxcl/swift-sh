600d613e9e96eabe10327fbb3f609b6a057ce90f
Max Howell <mxcl@me.com>
2024-09-09 13:18:47 -0400

    wip

--- Tests/All/IntegrationTests.swift
+++ Tests/All/IntegrationTests.swift
@@ -513,7 +513,7 @@ class TestingTheTests: XCTestCase {
             #if swift(>=6.0)
                 print(6.0)
             #elseif swift(>=5.10)
-                print(5.10)
+                print("5.10")
             #elseif swift(>=5.9)
                 print(5.9)
             #elseif swift(>=5.8)
